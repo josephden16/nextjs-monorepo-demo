e701c289aceb0daafb01f204d92883af6f70e79a
Denedo Oghenetega Joseph <tegadev01@gmail.com>
2022-10-10 16:44:16 +0000

    Start

---
+++ apps/admin/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { NextPage } from "next";
+
+const Admin: NextPage = () => {
+  return (
+    <div>
+      <h1>Admin</h1>
+      <button>Click Me!</button>
+    </div>
+  );
+};
+
+export default Admin;
---
+++ apps/store/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { NextPage } from "next";
+
+const Store: NextPage = () => {
+  return (
+    <div>
+      <h1>Store</h1>
+      <button>Click Me!</button>
+    </div>
+  );
+};
+
+export default Store;
